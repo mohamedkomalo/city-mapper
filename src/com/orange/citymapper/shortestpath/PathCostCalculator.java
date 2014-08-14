8d076657c9bf5c779c6aa4c723b87d83cc34213f
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-08-14 18:02:57 +0300

    added class 'PathCostCalculator' to be used in implementing CostQuery

---
+++ src/com/orange/citymapper/shortestpath/PathCostCalculator.java
@@ -0,0 +1,29 @@
+package com.orange.citymapper.shortestpath;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class PathCostCalculator {
+
+	public long calculatePathCost(Map<String, Map<String, Integer>> graph, List<String> nodes) {
+		long totalCost = 0;
+		
+		Iterator<String> iterator = nodes.iterator();
+		
+		String previousNode = iterator.next();
+		String currentNode;
+		
+		while (iterator.hasNext()) {
+			currentNode = iterator.next();
+			
+			int currentWeight = graph.get(previousNode).get(currentNode);
+			totalCost += currentWeight;
+			
+			previousNode = currentNode;
+		}
+		
+		return totalCost;
+	}
+
+}
---
+++ test/com/orange/citymapper/shortestpath/PathCostCalculatorTest.java
@@ -0,0 +1,41 @@
+package com.orange.citymapper.shortestpath;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class PathCostCalculatorTest {
+
+	@Test
+	public void testCalcuatePathCost() {
+		Map<String, Map<String, Integer>> graph = new HashMap<>();
+		graph.put("Cairo", new HashMap<String, Integer>());
+		graph.put("Tanta", new HashMap<String, Integer>());
+		graph.put("Kenna", new HashMap<String, Integer>());
+		graph.put("Sinai", new HashMap<String, Integer>());
+		graph.put("Alexandria", new HashMap<String, Integer>());
+		
+		graph.get("Cairo").put("Alexandria",200);
+		graph.get("Cairo").put("Tanta",500);
+		graph.get("Alexandria").put("Kenna",100);
+		graph.get("Alexandria").put("Sinai",700);
+		graph.get("Kenna").put("Tanta",100);
+		graph.get("Tanta").put("Kenna",100);
+		graph.get("Tanta").put("Sinai",300);
+		
+		PathCostCalculator pathCalculator = new PathCostCalculator();
+		
+		assertEquals(200,
+					pathCalculator.calculatePathCost(graph, Arrays.asList("Cairo", "Alexandria")));
+		
+		assertEquals(600,
+					pathCalculator.calculatePathCost(graph, Arrays.asList("Cairo", "Tanta", "Kenna")));
+		
+		assertEquals(200,
+					pathCalculator.calculatePathCost(graph, Arrays.asList("Alexandria", "Kenna", "Tanta")));
+	}
+}
