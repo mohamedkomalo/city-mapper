af5fa25297213694d1c789e3626bac5fadfacbf4
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-08-14 07:26:29 +0300

    postpended "ShortestPathQuery.getResult" test and implementation to later and implemented on "getSource" and "getDestination

--- src/com/orange/citymapper/queries/ShortestPathQuery.java
+++ src/com/orange/citymapper/queries/ShortestPathQuery.java
@@ -1,15 +1,54 @@
 package com.orange.citymapper.queries;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.orange.citymapper.data.Graph;
 
 public class ShortestPathQuery {
 
+	private static final int DEST_NUMBER = 2;
+	private static final int SOURCE_NUMBER = 1;
+	private static final String QUERY_REGEXP_PATTERN = "^What is the shortest path between City (\\S+) to City (\\S+)\\?$";
+
 	public boolean checkCorrectQuery(String queryString) {
-		return queryString.matches("^What is the shortest path between City (\\S+) to City (\\S+)\\?$");
+		return queryString.matches(QUERY_REGEXP_PATTERN);
 	}
 
 	public String getResult(String string, Graph graph) {
+		Pattern queryPattern = Pattern.compile(QUERY_REGEXP_PATTERN);
+		Matcher matcher = queryPattern.matcher(string);
+		
+		matcher.find();
+		String sourceCity = string.substring(matcher.regionStart(), matcher.regionEnd());
+		
+		matcher.find();
+		String destinationCity = string.substring(matcher.regionStart(), matcher.regionEnd());
+				
 		return null;
+	}
+
+	public String getSource(String string) {
+		return  extractCity(string, SOURCE_NUMBER);
+	}
+
+	public String getDestination(String string) {
+		return extractCity(string, DEST_NUMBER);
+	}
+
+	/**
+	 * @param queryString
+	 * @param cityNumber TODO
+	 * @return
+	 */
+	private String extractCity(String queryString, int cityNumber) {
+		Pattern queryPattern = Pattern.compile(QUERY_REGEXP_PATTERN);
+		Matcher matcher = queryPattern.matcher(queryString);
+		
+		matcher.find();
+		
+		String destination = matcher.group(cityNumber);
+		return destination;
 	}
 
 }
--- test/com/orange/citymapper/queries/ShortestPathQueryTests.java
+++ test/com/orange/citymapper/queries/ShortestPathQueryTests.java
@@ -28,29 +28,47 @@ public class ShortestPathQueryTests {
 		
 		assertFalse(result);
 	}
-	
+
 	@Test
-	public void testGetResult(){
+	public void testGetSource(){
 		ShortestPathQuery shortestPathQuery = new ShortestPathQuery();
 		
-		Graph graph = new Graph();
-		City cairo = new City("Cairo"),
-			 alexendria = new City("Alexendria"),
-			 tanta = new City("Tanta"),
-			 kenna = new City("Kenna"),
-			 sinai = new City("Sinai");
+		String sourceCity = shortestPathQuery.getSource("What is the shortest path between City Cairo to City Kenna?");
 		
-		graph.addEdge(new Edge(cairo, alexendria, 200));
-		graph.addEdge(new Edge(cairo, tanta, 500));
-		graph.addEdge(new Edge(alexendria, kenna, 100));
-		graph.addEdge(new Edge(alexendria, sinai, 700));
-		graph.addEdge(new Edge(kenna, tanta, 100));
-		graph.addEdge(new Edge(tanta, kenna, 100));
-		graph.addEdge(new Edge(tanta, sinai, 300));
-	
-		String queryResult = shortestPathQuery.getResult("what is the shortest path between Cairo to Sinai?", graph);
-		
-		assertEquals("The shortest path between Cairo to Sinai is 700", queryResult);
+		assertEquals("Cairo", sourceCity);
 	}
+	
+	@Test
+	public void testGetDestination(){
+		ShortestPathQuery shortestPathQuery = new ShortestPathQuery();
+		
+		String destinationCity = shortestPathQuery.getDestination("What is the shortest path between City Cairo to City Kenna?");
+		
+		assertEquals("Kenna", destinationCity);
+	}
+	
+//	@Test
+//	public void testGetResult(){
+//		ShortestPathQuery shortestPathQuery = new ShortestPathQuery();
+//		
+//		Graph graph = new Graph();
+//		City cairo = new City("Cairo"),
+//			 alexendria = new City("Alexendria"),
+//			 tanta = new City("Tanta"),
+//			 kenna = new City("Kenna"),
+//			 sinai = new City("Sinai");
+//		
+//		graph.addEdge(new Edge(cairo, alexendria, 200));
+//		graph.addEdge(new Edge(cairo, tanta, 500));
+//		graph.addEdge(new Edge(alexendria, kenna, 100));
+//		graph.addEdge(new Edge(alexendria, sinai, 700));
+//		graph.addEdge(new Edge(kenna, tanta, 100));
+//		graph.addEdge(new Edge(tanta, kenna, 100));
+//		graph.addEdge(new Edge(tanta, sinai, 300));
+//	
+//		String queryResult = shortestPathQuery.getResult("what is the shortest path between Cairo to Sinai?", graph);
+//
+//		assertEquals("The shortest path between Cairo to Sinai is 700", queryResult);
+//	}
 
 }
