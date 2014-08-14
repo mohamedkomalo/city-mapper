18a0cbb4c9f933e40ff52d73637bdabd6130f292
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-08-14 18:25:00 +0300

    implemented RouteQuery

--- src/com/orange/citymapper/queries/RouteQuery.java
+++ src/com/orange/citymapper/queries/RouteQuery.java
@@ -1,21 +1,75 @@
 package com.orange.citymapper.queries;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.orange.citymapper.data.Graph;
+import com.orange.citymapper.shortestpath.Dikstra;
+import com.orange.citymapper.shortestpath.Dikstra.Path;
 
 public class RouteQuery implements IQuery {
 
+	private static final String QUERY_REGEXP_PATTERN = "^Give me route between (\\S+) to (\\S+)\\?$";
+	/* TODO: replace numbers with group names and use a good naming for the variable
+	 * and extract getSource and getDest in a class of their own
+	 */
+	private static final int DEST_NUMBER = 2;
+	private static final int SOURCE_NUMBER = 1;
+	
 	@Override
 	public boolean checkCorrectQuery(String queryString)  {
-		return queryString.matches("^Give me route between (\\S+) to (\\S+)\\?$");
+		return queryString.matches(QUERY_REGEXP_PATTERN);
 	}
 
 	@Override
-	public String getResult(String string, Graph graph) {
-		// TODO Auto-generated method stub
-		return null;
+	public String getResult(String query, Graph graph) {
+		String source = getSource(query),
+			   destination = getDestination(query);
+		
+		Dikstra dikstra = new Dikstra();
+		Path resultPath = dikstra.getShortestPath(graph.getAdjacenceyMap(), source, destination);
+		String result;
+
+		if(resultPath != null){
+			result = resultPath.getNodes().toString();
+			result = result.replaceAll("\\[", "");
+			result = result.replaceAll("\\]", "");
+			result = result.replaceAll(", ", " ");
+		}
+		else
+			result = "query is wrong sir";
+		
+		/*
+		 *  TODO replacing using regex
+		 *  and extract the Query is wrong message in the IQuery interfce
+		 */
+		
+		return result;
 	}
 
-	
+
+	public String getSource(String string) {
+		return  extractCity(string, SOURCE_NUMBER);
+	}
+
+	public String getDestination(String string) {
+		return extractCity(string, DEST_NUMBER);
+	}
+
+	/**
+	 * @param queryString
+	 * @param cityNumber TODO
+	 * @return
+	 */
+	private String extractCity(String queryString, int cityNumber) {
+		Pattern queryPattern = Pattern.compile(QUERY_REGEXP_PATTERN);
+		Matcher matcher = queryPattern.matcher(queryString);
+		
+		matcher.find();
+		
+		String destination = matcher.group(cityNumber);
+		return destination;
+	}
 	
 
 }
--- test/com/orange/citymapper/queries/RouteQueryTests.java
+++ test/com/orange/citymapper/queries/RouteQueryTests.java
@@ -4,46 +4,58 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.orange.citymapper.data.City;
+import com.orange.citymapper.data.Edge;
+import com.orange.citymapper.data.Graph;
+
 public class RouteQueryTests {
+
+	private static final String KENNA_NAME = "Kenna";
+	private static final String SINAI_NAME = "Sinai";
+	private static final String TANTA_NAME = "Tanta";
+	private static final String ALEXENDRIA_NAME = "Alexendria";
+	private static final String CAIRO_NAME = "Cairo";
 
 	@Test
 	public void testRouteQueryWithValidCities() {
 		RouteQuery routeQuery = new RouteQuery();
-		
+
 		boolean result = routeQuery.checkCorrectQuery("Give me route between Alexandria to Sinai?");
-		
+
 		assertTrue(result);
-		}
+	}
+
 	@Test
 	public void testRouteQueryWihInvalidQuerySyntax() {
 		CostQuery costQuery = new CostQuery();
-		
+
 		boolean result = costQuery.checkCorrectQuery("In the sea there are fishes");
-		
+
 		assertFalse(result);
-		}
-//	@Test
-//	public void testGetResult(){
-//		RouteQuery routeQuery = new RouteQuery();
-//		
-//		Graph graph = new Graph();
-//		City cairo = new City("Cairo"),
-//			 alexendria = new City("Alexendria"),
-//			 tanta = new City("Tanta"),
-//			 kenna = new City("Kenna"),
-//			 sinai = new City("Sinai");
-//		
-//		graph.addEdge(new Edge(cairo, alexendria, 200));
-//		graph.addEdge(new Edge(cairo, tanta, 500));
-//		graph.addEdge(new Edge(alexendria, kenna, 100));
-//		graph.addEdge(new Edge(alexendria, sinai, 700));
-//		graph.addEdge(new Edge(kenna, tanta, 100));
-//		graph.addEdge(new Edge(tanta, kenna, 100));
-//		graph.addEdge(new Edge(tanta, sinai, 300));
-//	
-//		String queryResult = routeQuery.getResult("Give me route between Alexandria to Sinai?", graph);
-//		assertEquals("Alexandria Kenna Tanta Sinai", queryResult);
-//		
-//	}
+	}
+
+	@Test
+	public void testGetResult(){
+		RouteQuery routeQuery = new RouteQuery();
+
+		Graph graph = new Graph();
+		City cairo = new City(CAIRO_NAME),
+				alexendria = new City(ALEXENDRIA_NAME),
+				tanta = new City(TANTA_NAME),
+				kenna = new City(KENNA_NAME),
+				sinai = new City(SINAI_NAME);
+
+		graph.addEdge(new Edge(cairo, alexendria, 200));
+		graph.addEdge(new Edge(cairo, tanta, 500));
+		graph.addEdge(new Edge(alexendria, kenna, 100));
+		graph.addEdge(new Edge(alexendria, sinai, 700));
+		graph.addEdge(new Edge(kenna, tanta, 100));
+		graph.addEdge(new Edge(tanta, kenna, 100));
+		graph.addEdge(new Edge(tanta, sinai, 300));
+
+		String queryResult = routeQuery.getResult(String.format("Give me route between %s to %s?", ALEXENDRIA_NAME, TANTA_NAME), graph);
+		assertEquals(String.format("%s %s %s", ALEXENDRIA_NAME, KENNA_NAME, TANTA_NAME), queryResult);
+
+	}
 
 }
