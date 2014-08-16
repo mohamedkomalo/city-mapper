2a5f43e85c4a5ae1740a37722fbf25ccd197d0bb
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-08-16 12:15:22 +0300

    added test to DikstraTests

--- test/com/orange/citymapper/graph/algorithms/DikstraTest.java
+++ test/com/orange/citymapper/graph/algorithms/DikstraTest.java
@@ -33,6 +33,28 @@ public class DikstraTest {
 		assertEquals(200, dikstra.getShortestPath(graph, "Cairo", "Alexandria").getCost());
 		assertEquals(500, dikstra.getShortestPath(graph, "Alexandria", "Sinai").getCost());
 	}
+
+	@Test
+	public void testGetShortestPathWithSourceEqualDestination() {
+		Map<String, Map<String, Integer>> graph = new HashMap<>();
+		graph.put("Cairo", new HashMap<String, Integer>());
+		graph.put("Tanta", new HashMap<String, Integer>());
+		graph.put("Kenna", new HashMap<String, Integer>());
+		graph.put("Sinai", new HashMap<String, Integer>());
+		graph.put("Alexandria", new HashMap<String, Integer>());
+		
+		graph.get("Cairo").put("Alexandria",200);
+		graph.get("Cairo").put("Tanta",500);
+		graph.get("Alexandria").put("Kenna",100);
+		graph.get("Alexandria").put("Sinai",700);
+		graph.get("Kenna").put("Tanta",100);
+		graph.get("Tanta").put("Kenna",100);
+		graph.get("Tanta").put("Sinai",300);
+		
+		Dikstra dikstra = new Dikstra();
+
+		assertEquals(0, dikstra.getShortestPath(graph, "Cairo", "Cairo").getCost());
+	}
 	
 	@Test
 	public void testGetShortestPathWithInvalidPath() {
