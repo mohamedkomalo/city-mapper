a6822b01f515b7a42d2c18f89a153721d9f2ef94
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-08-16 11:36:28 +0300

    removed setters for source and destination objects, the setters must maintain the state in the Graph to match the new source and destination, since we don't do that know, the setters are removed

--- src/com/orange/citymapper/data/Edge.java
+++ src/com/orange/citymapper/data/Edge.java
@@ -17,14 +17,8 @@ public class Edge {
 	public City getSource() {
 		return source;
 	}
-	public void setSource(City source) {
-		this.source = source;
-	}
 	public City getDestination() {
 		return destination;
-	}
-	public void setDestination(City destination) {
-		this.destination = destination;
 	}
 	public int getCost() {
 		return cost;
