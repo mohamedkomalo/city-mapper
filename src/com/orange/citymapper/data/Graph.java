1844b6d7bd030e94d2de6d10bce92c49bfb33bc1
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-08-16 11:31:08 +0300

    refactored Graph

--- src/com/orange/citymapper/data/Graph.java
+++ src/com/orange/citymapper/data/Graph.java
@@ -6,29 +6,16 @@ import java.util.Map;
 import com.orange.citymapper.adapters.GraphAdapter;
 
 public class Graph {
-	Map<String, Map<String, Edge>> graph = new HashMap<>();
+	private Map<String, Map<String, Edge>> graph = new HashMap<>();
 	
 	public void addEdge(Edge edge) {
-		/*
-		 *  TODO refactor: extract methods like addCity, containsCity, rename
-		 *  class to something like map, rename function to something like addRoad
-		 *  overall rename to names that relate to the problem domain 
-		 */
+		addCityIfNotExists(edge.getSource());
+		addCityIfNotExists(edge.getDestination());
+		
 		String destName = edge.getDestination().getName();
 		String srcName = edge.getSource().getName();
 		
-		if(!graph.containsKey(destName)){
-			graph.put(destName, new HashMap<String, Edge>());
-		}
-		
-		Map<String, Edge> adjacentMap = graph.get(srcName);
-		
-		if(adjacentMap == null){
-			adjacentMap = new HashMap<>();
-			graph.put(srcName, adjacentMap);
-		}
-		
-		adjacentMap.put(destName, edge);
+		graph.get(srcName).put(destName, edge);
 	}
 
 	public Edge getEdge(String sourceName, String destName) {
@@ -40,17 +27,29 @@ public class Graph {
 		return adjacentMap.get(destName);
 	}
 	
-	@Override
-	public boolean equals(Object obj) {
-		if(obj instanceof Graph){
-			Graph obj2 = (Graph)obj;
-			return graph.equals(obj2.graph);
-		}
-		return super.equals(obj);
-	}
-	
 	public Map<String, Map<String, Integer>> getAdjacenceyMap(){
 		return new GraphAdapter(graph);
 	}
-
+	
+	private void addCity(City city){
+		graph.put(city.getName(), new HashMap<String, Edge>());
+	}
+	
+	private void addCityIfNotExists(City city){
+		if(!this.containsCity(city)){
+			this.addCity(city);
+		}		
+	}
+	
+	private boolean containsCity(City city){
+		return graph.containsKey(city.getName());
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Graph){
+			return graph.equals(((Graph)obj).graph);
+		}
+		return super.equals(obj);
+	}
 }
